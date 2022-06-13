4147fe643282d9f5c50e3f887994c84a11c3f0a3
Mitchell Tillman <mtillman14@gmail.com>
2022-06-12 21:24:57 -0400

    Updated getComputerID
    Uses Yair Altman's method from https://undocumentedmatlab.com/blog_old/unique-computer-id#comment-511840

--- Everything App/App Creation & Component Management/Helper Functions/getComputerID.m
+++ Everything App/App Creation & Component Management/Helper Functions/getComputerID.m
@@ -1,15 +1,17 @@
 function [sid]=getComputerID()
 
 %% PURPOSE: GET A UNIQUE IDENTIFIER FOR EACH COMPUTER
+% Taken from the "Other platforms" section of this Undocumented MATLAB page: https://undocumentedmatlab.com/blog_old/unique-computer-id#comment-511840
 
 sid = '';
 ni = java.net.NetworkInterface.getNetworkInterfaces;
 while ni.hasMoreElements
     addr = ni.nextElement.getHardwareAddress;
     if ~isempty(addr)
-        addrStr = dec2hex(int16(addr)+128);
-        sid = [sid, '.', reshape(addrStr,1,2*length(addr))];
+        sid = [sid, '.', sprintf('%.2X', typecast(addr, 'uint8'))]; % Modification from user comment by Jan Simon
+%         addrStr = dec2hex(int16(addr)+128); % Original line by Yair Altman
+%         sid = [sid, '.', reshape(addrStr,1,2*length(addr))]; % Original line by Yair Altman
     end
 end
 
-sid=getvarname(sid);
+sid=genvarname(sid);

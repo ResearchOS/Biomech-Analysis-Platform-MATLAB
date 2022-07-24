2ff83c2d4842fec54aaaf7d6ed729bfc226744a9
Mitchell Tillman <mtillman14@gmail.com>
2022-07-24 15:27:49 -0400

    Reorganized setSavedVars_Logsheet

--- Everything App/App Creation & Component Management/Helper Functions/setSavedVarsList_Logsheet.m
+++ Everything App/App Creation & Component Management/Helper Functions/setSavedVarsList_Logsheet.m
@@ -7,7 +7,7 @@ function []=setSavedVarsList_Logsheet(projectName,splitName,guiNames)
 % projectName: Name of the current project (char)
 % splitName: The name of the current split to save the variables to (char)
 % guiNames: The names of the variables as specified in the GUI (cell array
-% of char)
+% of chars)
 
 fig=evalin('base','gui;');
 projectSettingsMATPath=getappdata(fig,'projectSettingsMATPath');
@@ -33,38 +33,42 @@ splitCode=genSplitCode;
 
 numVars=length(guiNames);
 
-if exist('VariableNamesList','var')~=1 % Initialize the VariableNamesList    
+if exist('VariableNamesList','var')~=1 % Initialize the VariableNamesList
     VariableNamesList.GUINames=guiNames;
     VariableNamesList.SaveNames=guiVarNames;
     VariableNamesList.SplitNames=repmat(splitName,numVars,1);
     VariableNamesList.SplitCodes=repmat(splitCode,numVars,1);
     VariableNamesList.Descriptions=repmat({''},numVars,1);
-else
-    % Check if the split already exists. If so, check if those variables
-    % already exist. Either overwrite, or append to VariableNamesList
-    % depending
-
-    if ~ismember(splitName,VariableNamesList.SplitNames) % Split name does not yet exist
-        VariableNamesList.GUINames=[VariableNamesList.GUINames; guiNames];
-        VariableNamesList.SaveNames=[VariableNamesList.SaveNames; guiVarNames]; % Does not include the split code
-        VariableNamesList.SplitNames=[VariableNamesList.Splits; repmat(splitName,numVars,1)];
-        VariableNamesList.SplitCodes=repmat(splitCode,numVars,1);
-        VariableNamesList.Descriptions=[VariableNamesList.Descriptions; repmat({''},numVars,1)];
-
-    else % Split name already exists
-        % Check if any/all of the variables being saved are part of the
-        % split already
-
-        % Get the idx of the variables not already in the split
-        currVarsNotInSplit=~(ismember(splitName,VariableNamesList.SplitNames) & ismember(guiNames,VariableNamesList.GUINames));
-
-        VariableNamesList.GUINames=[VariableNamesList.GUINames; guiNames(currVarsNotInSplit)];
-        VariableNamesList.SaveNames=[VariableNamesList.SaveNames; guiVarNames(currVarsNotInSplit)]; % Does not include the split code
-        VariableNamesList.SplitNames=[VariableNamesList.SplitNames; repmat(splitName,sum(currVarsNotInSplit),1)];
-        VariableNamesList.SplitCodes=[VariableNamesList.SplitCodes; repmat(splitCode,sum(currVarsNotInSplit),1)];
-        VariableNamesList.Descriptions=[VariableNamesList.Descriptions; repmat({''},numVars,1)];
-
-    end
+    save(projectSettingsMATPath,'VariableNamesList','-append');
+    return;
 end
+
+% Check if the split already exists. If so, check if those variables
+% already exist. Either overwrite, or append to VariableNamesList
+% depending
+
+%% Split name does not yet exist
+if ~ismember(splitName,VariableNamesList.SplitNames)
+    VariableNamesList.GUINames=[VariableNamesList.GUINames; guiNames];
+    VariableNamesList.SaveNames=[VariableNamesList.SaveNames; guiVarNames]; % Does not include the split code
+    VariableNamesList.SplitNames=[VariableNamesList.SplitNames; repmat(splitName,numVars,1)];
+    VariableNamesList.SplitCodes=[VariableNamesList.SplitCodes; repmat(splitCode,numVars,1)];
+    VariableNamesList.Descriptions=[VariableNamesList.Descriptions; repmat({''},numVars,1)];
+    save(projectSettingsMATPath,'VariableNamesList','-append');
+    return;
+end
+
+%% Split name already exists
+% Check if any/all of the variables being saved are part of the
+% split already
+
+% Get the idx of the variables not already in the split
+currVarsNotInSplit=~(ismember(splitName,VariableNamesList.SplitNames) & ismember(guiNames,VariableNamesList.GUINames));
+
+VariableNamesList.GUINames=[VariableNamesList.GUINames; guiNames(currVarsNotInSplit)];
+VariableNamesList.SaveNames=[VariableNamesList.SaveNames; guiVarNames(currVarsNotInSplit)]; % Does not include the split code
+VariableNamesList.SplitNames=[VariableNamesList.SplitNames; repmat(splitName,sum(currVarsNotInSplit),1)];
+VariableNamesList.SplitCodes=[VariableNamesList.SplitCodes; repmat(splitCode,sum(currVarsNotInSplit),1)];
+VariableNamesList.Descriptions=[VariableNamesList.Descriptions; repmat({''},numVars,1)];
 
 save(projectSettingsMATPath,'VariableNamesList','-append');

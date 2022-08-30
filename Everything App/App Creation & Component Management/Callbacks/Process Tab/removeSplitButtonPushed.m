2b3aab911814653dce5b37c0ff698a3136a0e8fb
Mitchell Tillman <mtillman14@gmail.com>
2022-08-30 19:41:54 -0400

    Can now delete splits from the list!

--- Everything App/App Creation & Component Management/Callbacks/Process Tab/removeSplitButtonPushed.m
+++ Everything App/App Creation & Component Management/Callbacks/Process Tab/removeSplitButtonPushed.m
@@ -1,3 +1,38 @@
 function []=removeSplitButtonPushed(src,event)
 
-%% PURPOSE:
+%% PURPOSE: DELETE A PROCESSING SPLIT FROM THE LIST IN THE SPLITSUITREE. MUST NOT BE USED ANYWHERE IN THE MAP, OR HAVE ANY CHILDREN SPLITS.
+
+fig=ancestor(src,'figure','toplevel');
+handles=getappdata(fig,'handles');
+
+selNode=handles.Process.splitsUITree.SelectedNodes;
+splitText=selNode.Text;
+spaceIdx=strfind(splitText,' ');
+splitCode=splitText(spaceIdx+2:end-1);
+
+if ~isempty(selNode.Children)
+    disp('Cannot delete a split that has children!');
+    return;
+end
+
+projectSettingsMATPath=getappdata(fig,'projectSettingsMATPath');
+load(projectSettingsMATPath,'Digraph','NonFcnSettingsStruct');
+
+if ismember({splitCode},Digraph.Edges.SplitCode)
+    disp('Cannot delete a split that still has existing edges!');
+    return;
+end
+
+splitList=getSplitsOrder(selNode,handles.Process.splitsUITree.Tag);
+structPath='NonFcnSettingsStruct.Process.Splits';
+for i=1:length(splitList)-1
+    structPath=[structPath '.SubSplitNames.(' splitList{i}];
+end
+
+structPath=[structPath '.SubSplitNames'];
+
+eval([structPath '=rmfield(' structPath ',''' splitList{end} ''');']);
+
+delete(selNode);
+
+save(projectSettingsMATPath,'NonFcnSettingsStruct','-append');

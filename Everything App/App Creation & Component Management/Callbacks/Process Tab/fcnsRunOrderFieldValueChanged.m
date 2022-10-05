77f1cfbf31cc7fda34275a58cfccb8c8cd0e22b9
Mitchell Tillman <mtillman14@gmail.com>
2022-10-05 19:00:02 -0400

    Now setting the function run order auto increments the rest of the chain, until there is ambiguity (i.e. branching).
    Only acts within one split. If multiple splits but only one branch on that split, still auto names.

--- Everything App/App Creation & Component Management/Callbacks/Process Tab/fcnsRunOrderFieldValueChanged.m
+++ Everything App/App Creation & Component Management/Callbacks/Process Tab/fcnsRunOrderFieldValueChanged.m
@@ -38,15 +38,18 @@ if isempty(nodeNum)
     return;
 end
 
-% fcnName=selNode.Text;
+numSelNodes=length(handles.Process.fcnArgsUITree.Children);
 
-projectSettingsMATPath=getappdata(fig,'projectSettingsMATPath');
+if numSelNodes>1
+    disp('Can only select one node at a time!');
+    return;
+end
+
 Digraph=getappdata(fig,'Digraph');
-% load(projectSettingsMATPath,'Digraph');
 
 % Get the unique index of the desired edge.
 % edgeIdx=ismember(Digraph.Edges.SplitCode,splitCode) & ismember(Digraph.Edges.NodeNumber(:,2),nodeNum);
-nodeRow=ismember(Digraph.Nodes.NodeNumber,nodeNum);
+nodeRow=find(ismember(Digraph.Nodes.NodeNumber,nodeNum)==1);
 
 prevOrderNum=Digraph.Nodes.RunOrder{nodeRow}.([splitName '_' splitCode]);
 
@@ -54,16 +57,54 @@ if orderNum==prevOrderNum
     return;
 end
 
-% Change the rest of the numbers to avoid duplicates.
-% if orderNum<prevOrderNum % Moving to a larger index, subtract one
-%     lessThanNewNumIdx=Digraph.Edges.RunOrder<orderNum & Digraph.Edges.RunOrder>prevOrderNum;
-%     Digraph.Edges.RunOrder(lessThanNewNumIdx)=Digraph.Edges.RunOrder(lessThanNewNumIdx)-1;
-% else % Moving to a smaller index, add one
-%     largerThanNewNumIdx=Digraph.Edges.RunOrder>orderNum & Digraph.Edges.RunOrder<prevOrderNum;
-%     Digraph.Edges.RunOrder(largerThanNewNumIdx)=Digraph.Edges.RunOrder(largerThanNewNumIdx)+1;
+Digraph.Nodes.RunOrder{nodeRow}.([splitName '_' splitCode])=orderNum;
+
+% inNodes=inedges(Digraph,nodeRow);
+% outNodes=outedges(Digraph,nodeRow);
+% 
+% if ~(length(inNodes)==1 && length(outNodes)==1)
+%     setappdata(fig,'Digraph',Digraph);
+% 
+%     if runLog
+%         desc='Change the order in which the functions are run';
+%         updateLog(fig,desc,orderNum);
+%     end
+%     return;
 % end
 
-Digraph.Nodes.RunOrder{nodeRow}.([splitName '_' splitCode])=orderNum;
+%% Propagate the change to the fcn run order
+isUnique=true; % Indicate that there is only one inedge and one outedge for this split
+while isUnique
+
+    nodeID=Digraph.Nodes.NodeNumber(nodeRow);
+
+    edgeRows=ismember(Digraph.Edges.NodeNumber(:,1),nodeID);
+
+%     nodeRow=outedges(Digraph,nodeRow); % The node just after the current node
+    if isempty(edgeRows)
+        isUnique=0;
+        break;
+    end
+
+%     edgeRows=find(ismember(Digraph.Edges.EndNodes(:,2),nodeRow)==1); % The rows of the edges coming out of the current node
+
+    edgeRowsCurrSplit=ismember(Digraph.Edges.SplitCode,splitCode);
+    currEdgeRowsCurrSplit=edgeRowsCurrSplit & edgeRows; % The rows in the current split that are outedges of the current node.
+    if sum(currEdgeRowsCurrSplit)~=1 % Either there are no edges of the current split, or there are more than one.
+        isUnique=0;
+        break;
+    end   
+
+    nextNodeID=Digraph.Edges.NodeNumber(currEdgeRowsCurrSplit,2);
+    nextNodeRow=ismember(Digraph.Nodes.NodeNumber,nextNodeID);
+        
+    orderNum=orderNum+1;
+    Digraph.Nodes.RunOrder{nextNodeRow}.([splitName '_' splitCode])=orderNum;
+
+    nodeRow=find(nextNodeRow==1);
+
+end
+
 
 % save(projectSettingsMATPath,'Digraph','-append');
 setappdata(fig,'Digraph',Digraph);

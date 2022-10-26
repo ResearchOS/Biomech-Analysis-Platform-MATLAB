642db9a3e78f5aa9feb39cd0748754e5ca3a5232
Mitchell Tillman <mtillman14@gmail.com>
2022-10-25 21:00:23 -0400

    Now deletes the plot object handles before saving to avoid unpleasantness.

--- Everything App/App Creation & Component Management/Callbacks/PGUI UIFigure/saveGUIState.m
+++ Everything App/App Creation & Component Management/Callbacks/PGUI UIFigure/saveGUIState.m
@@ -40,6 +40,26 @@ if exist(projectSettingsMATPath,'file')~=2 || isempty(varsList)
     return;
 end
 
+%% Delete all of the handles from Plotting struct so that it can be saved and loaded properly.
+if ~isempty(Plotting)
+    plotNames=fieldnames(Plotting.Plots);
+    for plotNum=1:length(plotNames)
+        plotName=plotNames{plotNum};
+        compNames=fieldnames(Plotting.Plots.(plotName));
+        compNames=compNames(~ismember(compNames,{'Movie','Metadata','SpecifyTrials','ExTrial'}));
+        for compNum=1:length(compNames)
+            compName=compNames{compNum};
+            letters=fieldnames(Plotting.Plots.(plotName).(compName));
+            for letNum=1:length(letters)
+                letter=letters{letNum};
+                disp([plotName ' ' compName ' ' letter]);
+                Plotting.Plots.(plotName).(compName).(letter).Handle=[];
+            end
+        end
+
+    end
+end
+
 save(projectSettingsMATPath,varsList{:},'-append');
 
 %% SAVE THE CURRENT PLOT

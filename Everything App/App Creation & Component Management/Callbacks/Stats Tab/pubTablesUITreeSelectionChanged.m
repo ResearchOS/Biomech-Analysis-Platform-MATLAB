a54d8d5c0dc1a0ca0347c36932e1d3963386a48a
Mitchell Tillman <mtillman14@gmail.com>
2022-11-07 14:07:25 -0500

    Significant figures now gets dynamically updated.

--- Everything App/App Creation & Component Management/Callbacks/Stats Tab/pubTablesUITreeSelectionChanged.m
+++ Everything App/App Creation & Component Management/Callbacks/Stats Tab/pubTablesUITreeSelectionChanged.m
@@ -1,6 +1,18 @@
 function []=pubTablesUITreeSelectionChanged(src,event)
 
-%% PURPOSE:
+%% PURPOSE: CHANGE UI ELEMENTS IN RESPONSE TO SELECTING DIFFERENT PUBLICATION TABLE.
 
 fig=ancestor(src,'figure','toplevel');
 handles=getappdata(fig,'handles');
+
+Stats=getappdata(fig,'Stats');
+
+if isempty(handles.Stats.pubTablesUITree.SelectedNodes)
+    return;
+end
+
+currPubTable=handles.Stats.pubTablesUITree.SelectedNodes.Text;
+
+numSigFigs=Stats.PubTables.(currPubTable).SigFigs;
+
+handles.Stats.numSigFigsEditField.Value=numSigFigs;

b73a9600b028a3a08fe615bb350f6fef520b215b
Mitchell Tillman <mtillman14@gmail.com>
2023-07-07 19:23:25 -0400

    Revert "getCommonPath is now computer-specific"
    This reverts commit 99757e613abf1b23e7cbbedb7323f4766730c051.

--- Everything App New/Helper Functions/getCommonPath.m
+++ Everything App New/Helper Functions/getCommonPath.m
@@ -1,18 +1,17 @@
 function [commonPath]=getCommonPath()
 
 %% PURPOSE: RETURN THE PATH TO THE DIRECTORY WHERE THE PGUI SETTINGS FILES ARE STORED.
+
 rootSettingsFile=getRootSettingsFile();
 
-%% Root settings contains all of the Settings variables
+%% Root settings simply contains the path to where all of the Settings variables are stored.
 try
     load(rootSettingsFile,'commonPath');
-    computerID = getComputerID();
-    commonPath = commonPath.(computerID);
 catch e
-    if ~ismember(e.identifier,{'MATLAB:load:couldNotReadFile','MATLAB:nonExistentField'}) % If the file does not exist or the computerID field is missing for this computer.
+    if ~isequal(e.identifier,'MATLAB:load:couldNotReadFile') % If the file does not exist.
         error(e); % Some other error occurred.
     end
 
     setCommonPath();
-    commonPath = getCommonPath;
+    commonPath = getCommonPath;    
 end

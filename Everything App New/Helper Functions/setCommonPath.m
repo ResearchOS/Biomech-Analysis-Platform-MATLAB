6b2eb908eb562682502a89b944e055d76b02cbb8
Mitchell Tillman <mtillman14@gmail.com>
2023-07-07 19:23:25 -0400

    Revert "getCommonPath is now computer-specific"
    This reverts commit 99757e613abf1b23e7cbbedb7323f4766730c051.

--- Everything App New/Helper Functions/setCommonPath.m
+++ Everything App New/Helper Functions/setCommonPath.m
@@ -1,26 +1,17 @@
 function []=setCommonPath(commonPath)
 
-%% PURPOSE: PROMPT THE USER FOR THE SETTINGS PATH. THIS IS STORED WITH THE COMPUTER'S ID TO FACILITATE SHARING THE SETTINGS FILE BETWEEN COMPUTERS.
+%% PURPOSE: PROMPT THE USER FOR THE SETTINGS PATH
+% handles=getappdata(fig,'handles');
 
 rootSettingsFile=getRootSettingsFile();
 
-computerID = getComputerID();
-
 if nargin==0
-    commonPathNew=uigetdir(cd,'Select Path to Save Settings');
-end
-
-try
-    load(rootSettingsFile,'commonPath');
-catch
-    
+    commonPath=uigetdir(cd,'Select Path to Save Settings');
 end
 
 if commonPath==0
-    commonPath.(computerID)=userpath; % If no common path is selected, it will just default to the MATLAB default userpath.
+    commonPath=userpath; % If no common path is selected, it will just default to the MATLAB default userpath.
 end
-
-commonPath.(computerID) = commonPathNew;
 
 try
     save(rootSettingsFile,'commonPath','-v6','-append');

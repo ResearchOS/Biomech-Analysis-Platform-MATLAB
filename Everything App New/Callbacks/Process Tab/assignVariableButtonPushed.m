06c12b3227d7ea1aa0937560d0a04d183426ce77
Mitchell Tillman <mtillman14@gmail.com>
2023-07-12 18:55:21 -0400

    Can now assign and unassign variables! Also add/remove args.
    Now just adding to queue, specify trials, and linking, before I can start to re-run the analyses.

--- Everything App New/Callbacks/Process Tab/assignVariableButtonPushed.m
+++ Everything App New/Callbacks/Process Tab/assignVariableButtonPushed.m
@@ -9,45 +9,38 @@ fig=ancestor(src,'figure','toplevel');
 handles=getappdata(fig,'handles');
 
 % All variables UI tree
-selNode=handles.Process.allVariablesUITree.SelectedNodes;
+allNode=handles.Process.allVariablesUITree.SelectedNodes;
 
-if isempty(selNode)
+if isempty(allNode)
     return;
 end
 
-currTab=handles.Tabs.tabGroup1.SelectedTab.Title;
+processUITree=handles.Process.functionUITree;
 
-switch currTab
-    case 'Process'
-        motherUITree=handles.Process.groupUITree;
-        daughterUITree=handles.Process.functionUITree;
-        daughterClass='Process';
-        motherClass='ProcessGroup';
-    case 'Plot'
-        motherUITree=handles.Plot.plotUITree;
-        daughterUITree=handles.Plot.componentUITree;
-        daughterClass='Component';
-        motherClass='Plot';
-    case 'Stats'
+% Current process group UI tree
+currFcnNode = handles.Process.groupUITree.SelectedNodes;
 
-end
-
-% Current group UI tree
-motherNode=motherUITree.SelectedNodes;
-
-if isempty(motherNode)
+if isempty(currFcnNode)
     return;
 end
 
 % Current function UI tree
-daughterNode=daughterUITree.SelectedNodes;
+currVarNode=processUITree.SelectedNodes;
 
-if isempty(daughterNode)
+if isempty(currVarNode)
     return;
 end
 
-selUUID = selNode.NodeData.UUID;
-[type, abstractID, instanceID] = deText(selUUID);
+currVarUUID = currVarNode.NodeData.UUID;
+
+parentNode = currVarNode.Parent;
+if isequal(parentNode, processUITree)
+    disp('Cannot select the getArg or setArg nodes! Must select the specific variables.');
+    return;
+end
+
+allVarUUID = allNode.NodeData.UUID;
+[type, abstractID, instanceID] = deText(allVarUUID);
 
 % Abstract selected. Create new instance.
 if isempty(instanceID)
@@ -56,155 +49,43 @@ if isempty(instanceID)
     if ~isequal(a,'Yes')
         return;
     end
-    varStruct = createNewObject(true, 'Variable', selNode.Text, abstractID, '', true);
-    selUUID = varStruct.UUID;
+    varStruct = createNewObject(true, 'Variable', allNode.Text, abstractID, '', true);
+    allVarUUID = varStruct.UUID;
     abstractUUID = genUUID(type, abstractID);
     absNode = selectNode(handles.Process.allVariablesUITree, abstractUUID);
 
     % Create the new node in the "all" UI tree
-    addNewNode(absNode, selUUID, varStruct.Text);
+    addNewNode(absNode, allVarUUID, varStruct.Text);
 end
 
+getSetArgIdxNum = str2double(parentNode.Text(isstrprop(parentNode.Text,'digit'))); % Number of this getArg or setArg
 
-
-
-if isequal(daughterNode.Parent,daughterUITree)
-    disp('Must select an individual argument, not the getArg/setArg parent node!');
-    return;
+if isequal(parentNode.Text(1:6),'getArg')
+    fldName = 'InputVariables';
+elseif isequal(parentNode.Text(1:6),'setArg')
+    fldName = 'OutputVariables';
 end
 
-if exist('varNameInCode','var')~=1
-    varNameInCode=daughterNode.Text;
-end
+currFcnUUID = currFcnNode.NodeData.UUID;
+fcnStruct = loadJSON(currFcnUUID);
 
-% Determine whether to create a new project-specific variable version or
-% use an existing one.
-if (isequal(selNode.Parent,handles.Process.allVariablesUITree) && isempty(selNode.Children)) % Special case where there are no existing PS versions.
-    isNew=true;
-else
-    isNew=false;
-end
-
-% PI node selected
-if isequal(selNode.Parent,handles.Process.allVariablesUITree) && ~isNew
-    if length(selNode.Children)==1
-        selNode=selNode.Children(1);
-    elseif length(selNode.Children)>1
-        disp('Multiple options, please select a project-specific option!');
-        expand(selNode);
-        return;
+getSetArgIdx = [];
+for i=1:length(fcnStruct.(fldName))
+    if isequal(fcnStruct.(fldName){i}{1},getSetArgIdxNum)
+        getSetArgIdx = i;
+        break;
     end
 end
+assert(~isempty(getSetArgIdx));
 
-% Get the currently selected PS daughter struct (class Process or Component)
-fullPath=getClassFilePath(motherNode.Text, daughterClass);
-daughterStruct_PS=loadJSON(fullPath);
-
-% Check if daughterNode already has a variable assigned that is being replaced. If so, need to unlink
-% that variable from the mother class object
-if contains(varNameInCode,' ')
-    spaceIdx=strfind(varNameInCode,' ');
-    varText=varNameInCode(spaceIdx+2:end-1);
-    prevVarPath=getClassFilePath(varText, 'Variable');
-    prevVarStruct=loadJSON(prevVarPath);
-    [prevVarStruct, daughterStruct_PS]=unlinkClasses(prevVarStruct, daughterStruct_PS);
-end
-
-% Get the ID number for which getArg/setArg is currently being modified.
-parentNode=daughterNode.Parent;
-parentText=parentNode.Text;
-spaceSplit=strsplit(parentText,' ');
-number=str2double(spaceSplit{2});
-
-% Get the index of the current getArg/setArg in the UI tree, which
-% matches the index in the file.
-childrenNodes=[daughterUITree.Children];
-childrenNodesTexts={childrenNodes.Text};
-argType=parentNode.Text(1:6);
-argSpecificIdx=contains(childrenNodesTexts,argType);
-argIdxNum=find(ismember(childrenNodes(argSpecificIdx), parentNode)==1);
-
-% Get the index of the arg being modified in that getArg/setArg
-% instance.
-idxNum=find(ismember(parentNode.Children,daughterNode)==1);
-
-if exist('varName','var')~=1
-    varName=selNode.Text;
-else
-    selNode=selectNode(handles.Process.allVariablesUITree,varName);
-end
-
-[name,id]=deText(varName);
-
-% Get the PI variable struct.
-piText=[name '_' id];
-varPathPI=getClassFilePath(piText,'Variable');
-varStructPI=loadJSON(varPathPI);
-
-projectSettingsFile=getProjectSettingsFile();
-projectSettings=loadJSON(projectSettingsFile);
-Current_ProcessGroup_Name=projectSettings.Current_ProcessGroup_Name;
-groupPath=getClassFilePath(Current_ProcessGroup_Name,'ProcessGroup');
-currGroup=loadJSON(groupPath);
-if ~isempty(currGroup.Tags)
-    tag=currGroup.Tags{end};
-else
-    tag='';
-end
-
-switch isNew
-    case true
-        % Create a new project-specific ID for the variable to add.
-        varStruct=createVariableStruct_PS(varStructPI,[],tag);
-    case false
-        varPath=getClassFilePath_PS(varName, 'Variable');
-        varStruct=loadJSON(varPath);
-end
-
-% If the variable is hard-coded, indicate that this version did not come from any process function.
-% Helpful because in the future I don't have to load the PI struct.
-% Potentially tough to debug!!!!!!
-if varStructPI.IsHardCoded
-    varStruct.BackwardLinks_Process={'HardCoded'};
-end
-
-% Apply the currently selected variable to that arg.
-if isequal(currTab,'Process')
-    if isequal(parentText(1:6),'getArg')
-        fldName='InputVariables';
-        varStruct.ForwardLinks_Process=unique([varStruct.ForwardLinks_Process; {daughterStruct_PS.Text}]);
-    elseif isequal(parentText(1:6),'setArg')
-        fldName='OutputVariables';
-        varStruct.BackwardLinks_Process=unique([varStruct.BackwardLinks_Process; {daughterStruct_PS.Text}]);
-    end
-else
-    fldName='InputVariables';
-end
-
-if ~iscell(daughterStruct_PS.(fldName)) || ...
-        (iscell(daughterStruct_PS.(fldName)) && (length(daughterStruct_PS.(fldName))<argIdxNum || isempty(daughterStruct_PS.(fldName){argIdxNum})))
-    daughterStruct_PS.(fldName){argIdxNum}{1}=number;
-end
-
-% Check that I'm putting things in the right place
-assert(isequal(daughterStruct_PS.(fldName){argIdxNum}{1},number));
-
-daughterStruct_PS.(fldName){argIdxNum}{idxNum+1}=varStruct.Text;
-
-% Saves changes.
+argIdx = ismember(fcnStruct.(fldName){getSetArgIdx}(2:end),currVarUUID); % Get which argument this is.
+argIdx = [false; argIdx];
+fcnStruct.(fldName){getSetArgIdx}(argIdx) = {allVarUUID};
 if isequal(fldName,'InputVariables')
-    linkClasses(varStruct, daughterStruct_PS); % Input variables are "ForwardLinks" to the Process function.
-elseif isequal(fldName,'OutputVariables')
-    linkClasses(daughterStruct_PS,varStruct); % Output variables are "BackwardLinks" to the Process function.
-else
-    writeJSON(varPath, varStruct); % Just save the changes.
+    fcnStruct.InputSubvariables{getSetArgIdx}(argIdx) = {''}; % Changing the variable, so the subvariable should be reset.
 end
+writeJSON(getJSONPath(fcnStruct), fcnStruct);
 
-% Modify/add nodes
-argTextSplit=strsplit(daughterNode.Text,' ');
-argText=[argTextSplit{1} ' (' varStruct.Text ')'];
-daughterNode.Text=argText;
-
-if isNew
-    uitreenode(selNode,'Text',varStruct.Text,'ContextMenu',handles.Process.psContextMenu);
-end
+currVarNode.NodeData.UUID = allVarUUID;
+argTextSplit = strsplit(currVarNode.Text);
+currVarNode.Text = [argTextSplit{1} ' (' allVarUUID ')'];

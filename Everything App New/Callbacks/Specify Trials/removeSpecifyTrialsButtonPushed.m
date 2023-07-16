b6b0ce9f138e395c9caa07252806fcbcb4a886a3
Mitchell Tillman <mtillman14@gmail.com>
2023-07-16 13:27:11 -0400

    Fixed archiving abstract objects. Also remove specify trials from all UI trees.

--- Everything App New/Callbacks/Specify Trials/removeSpecifyTrialsButtonPushed.m
+++ Everything App New/Callbacks/Specify Trials/removeSpecifyTrialsButtonPushed.m
@@ -24,4 +24,17 @@ uuid = selNode.NodeData.UUID;
 
 moveToArchive(uuid);
 
-delete(selNode);
+%% Remove the node from each ui tree
+tabs = {'Import','Process'};
+for i=1:length(tabs)
+    tab = tabs{i};
+    switch tab
+        case 'Import'
+            stClass = 'Logsheet';
+        case 'Process'
+            stClass = 'Process';
+    end
+    uiTree = getUITreeFromClass(fig, 'SpecifyTrials', 'all', stClass);
+    node = getNode(uiTree, uuid);
+    delete(node);
+end
